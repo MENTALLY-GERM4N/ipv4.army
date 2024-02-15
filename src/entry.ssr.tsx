a62873ab877afcc27013f5d6753fe0b32d4dd933
Unstream <git@wont.stream>
2024-02-14 19:08:00 -0500

    update

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -21,6 +21,7 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
+    base: "https://cdn.jsdelivr.net/gh/wont-stream/wont-stream.github.io/",
     // Use container attributes to set attributes on the html tag.
     containerAttributes: {
       lang: "en-us",
