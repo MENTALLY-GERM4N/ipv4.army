3cedbf860f6eba4ad5be9c2e86e4fdaa615e0308
Unstream <git@wont.stream>
2024-04-29 22:51:32 -0400

    formatting

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
 import "@material/web/iconbutton/filled-tonal-icon-button";
 import "@material/web/icon/icon";
 
-const app = document.getElementById("app")
+const app = document.getElementById("app");
 
 const links = [
-    {
-        name: "GitHub",
-        href: "https://i.wont.stream/github",
-        icon: "code"
-    },
-    {
-        name: "Twitch",
-        href: "https://i.wont.stream/twitch",
-        icon: "cast"
-    },
-    {
-        name: "Discord Server",
-        href: "https://i.wont.stream/discord",
-        icon: "chat"
-    },
-    {
-        name: "Reviews",
-        href: "/reviews",
-        icon: "reviews"
-    }
-    
-]
+  {
+    name: "GitHub",
+    href: "https://i.wont.stream/github",
+    icon: "code",
+  },
+  {
+    name: "Twitch",
+    href: "https://i.wont.stream/twitch",
+    icon: "cast",
+  },
+  {
+    name: "Discord Server",
+    href: "https://i.wont.stream/discord",
+    icon: "chat",
+  },
+  {
+    name: "Reviews",
+    href: "/reviews",
+    icon: "reviews",
+  },
+];
 
-let dom = ""
+let dom = "";
 
-dom += "<div class=\"links\">"
-links.forEach(link => {
-    dom += `<md-filled-tonal-icon-button type="a" href="${link.href}" aria-label="${link.name}"/>
+dom += '<div class="links">';
+links.forEach((link) => {
+  dom += `<md-filled-tonal-icon-button type="a" href="${link.href}" aria-label="${link.name}"/>
     <md-icon>${link.icon}</md-icon>
-  </md-filled-tonal-icon-button>`
-})
-dom += "<div \\>"
+  </md-filled-tonal-icon-button>`;
+});
+dom += "<div \\>";
 
-app.innerHTML += dom
+app.innerHTML += dom;
