c11f3fbbe47380292dc83c9a34ead060c268c5e9
wont-stream <src@wont.stream>
2024-05-05 16:36:07 -0400

    update

--- build.js
+++ build.js
@@ -1,3 +1,10 @@
+const fs = require("node:fs/promises");
+const https = require("node:https")
+
+https.get("https://cdn.jsdelivr.net/npm/@material-symbols/font-400@latest/material-symbols-rounded.woff2", async (resp) => {
+  resp.pipe(await fs.createWriteStream("material-symbols-rounded.woff2"))
+})
+
 await Bun.build({
   entrypoints: ["./src/index.js", "./src/reviews.js"],
   outdir: "./dist",
--- dev.js
+++ dev.js
@@ -1,4 +1,9 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
+const https = require("node:https")
+
+https.get("https://cdn.jsdelivr.net/npm/@material-symbols/font-400@latest/material-symbols-rounded.woff2", async (resp) => {
+  resp.pipe(await fs.createWriteStream("material-symbols-rounded.woff2"))
+})
 
 fs.watch(
   __dirname,
@@ -6,10 +11,8 @@ fs.watch(
     recursive: true,
   },
   async (e, file) => {
-    if (file.startsWith("src\\")) {
-      build();
-      console.log(`Rebuilt ${file}`);
-    }
+    if (file.startsWith("dist/")) return;
+    build();
   }
 );
 
