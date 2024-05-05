b6730f2cae46c35cd29743964de11e53b334b3bf
wont-stream <src@wont.stream>
2024-05-05 16:41:15 -0400

    :sad:

--- dev.js
+++ dev.js
@@ -1,8 +1,8 @@
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 const https = require("node:https")
 
 https.get("https://cdn.jsdelivr.net/npm/@material-symbols/font-400@latest/material-symbols-rounded.woff2", async (resp) => {
-  resp.pipe(await fs.createWriteStream("material-symbols-rounded.woff2"))
+  resp.pipe(fs.createWriteStream("material-symbols-rounded.woff2"))
 })
 
 fs.watch(
