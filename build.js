155f90f67b79c406cc93fdfdc7df1e0065ba8596
wont-stream <src@wont.stream>
2024-05-07 18:16:47 -0400

    oops

--- build.js
+++ build.js
@@ -2,10 +2,6 @@ import lightningcss from 'bun-lightningcss'
 import fs from "node:fs";
 import "./build/getFont";
 
-https.get("https://cdn.jsdelivr.net/npm/@material-symbols/font-400@latest/material-symbols-rounded.woff2", async (resp) => {
-  resp.pipe(fs.createWriteStream("material-symbols-rounded.woff2"))
-})
-
 await Bun.build({
   entrypoints: ["./src/index.js", "./src/reviews.js"],
   outdir: "./dist",
