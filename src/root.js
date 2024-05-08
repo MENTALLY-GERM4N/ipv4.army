fdb41b8208a33a82d7e1238e10f700d33b37fed2
wont-stream <src@wont.stream>
2024-05-07 23:43:08 -0400

    update

--- src/root.js
+++ src/root.js
@@ -1,14 +1,12 @@
 (() => {
   if (location.protocol !== "https:") return;
-  fetch("https://a.wont.stream/api/event", {
+  fetch("https://a.wont.stream/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      domain: "wont.stream",
-      name: "pageview",
-      url: window.location.href,
+      path: window.location.pathname,
       referrer: document.referrer,
     }),
   });
