e66dfa703cf07865572cdb08e8ebed27391a34a3
wont-stream <src@wont.stream>
2024-05-11 21:50:01 -0400

    upd

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -37,6 +37,8 @@ export const RouterHead = component$(() => {
     <meta name="theme-color" content="#000000" />
 
     <meta property="og:title" content="Unstream" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="/icons/apple-touch-icon.png" />
     <meta name="twitter:title" content="Unstream" />
     <meta
       property="og:description"
