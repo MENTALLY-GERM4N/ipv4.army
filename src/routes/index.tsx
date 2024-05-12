28b3a91b8a69ad023703ba2477c3df9bb628240c
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-05-12 08:41:34 -0400

    seo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Unstream.</h1>
+      <h2>Unstream.</h2>
       <h3>A self taught fullstack developer, with a love of backends.</h3>
     </>
   );
