7ced41d93408a83ef0950689c84df21ea2ddb9a2
wont-stream <src@wont.stream>
2024-05-14 15:49:37 -0400

    api

---
+++ src/routes/api/badge/index.tsx
@@ -0,0 +1,14 @@
+import { type RequestHandler } from '@builder.io/qwik-city';
+import badge from 'shields.io/badge-maker'; 
+
+export const onGet: RequestHandler = async (res) => {
+  res.headers.set("content-type", "image/svg+xml;charset=utf-8")
+
+  let options = {}
+  
+  res.query.forEach((v,k) => {
+    options[k] = v
+  })
+
+  res.send(200, badge.makeBadge(options))
+};
