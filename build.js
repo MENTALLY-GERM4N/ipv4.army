90c2e1bf49fe1663561a96cc43ab1cd57c48999b
Unstream <drns.pw@icloud.com>
2024-06-06 15:30:33 -0400

    update

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ let html = await Bun.file("./src/index.html").text();
 html = html.replace("{{HREF}}", "href");
 html = html.replace("{{VERSION}}", pkg.version);
 
-html = html.replace(/\.\/scripts\/(.*?)\.js/g, "/scripts/$1.min.js");
-html = html.replace(/\.\/scripts\/(.*?)\.js/g, "/scripts/$1.min.js");
+html = html.replace(/scripts\/(.*?)\.js/g, "scripts/$1.min.js");
+html = html.replace(/styles\/(.*?)\.css/g, "styles/$1.min.css");
 
 Bun.write("./src/index.html", html);
