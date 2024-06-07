cd197c8cfc5f5e6410312a336b3ba94fdfffe0d6
Unstream <drns.pw@icloud.com>
2024-06-07 16:35:17 -0400

    oml

--- lastStep.js
+++ lastStep.js
@@ -4,6 +4,7 @@ import fs from "fs";
   let file = fs.readFileSync("./dist/index.html").toString();
 
   file = file.replaceAll(`.js"`, `.min.js"`);
+  file = file.replaceAll(`.css"`, `.min.css"`);
   file = file.replaceAll(`service-worker.min.js"`, `service-worker.js"`);
 
   fs.writeFileSync("./dist/index.html", file);
@@ -12,6 +13,7 @@ import fs from "fs";
   let file = fs.readFileSync("./dist/service-worker.js").toString();
 
   file = file.replaceAll(`.js"`, `.min.js"`);
+  file = file.replaceAll(`.css"`, `.min.css"`);
 
   fs.writeFileSync("./dist/service-worker.js", file);
 }
