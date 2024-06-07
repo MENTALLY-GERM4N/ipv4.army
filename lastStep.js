8c2b7ce6df4bdf61fc054fca94b10bb4a989bc66
Unstream <drns.pw@icloud.com>
2024-06-07 16:29:46 -0400

    ARG

--- lastStep.js
+++ lastStep.js
@@ -1,7 +1,16 @@
 import fs from "fs";
 
-let file = fs.readFileSync("./dist/index.html").toString();
+{
+  let file = fs.readFileSync("./dist/index.html").toString();
 
-file = file.replaceAll(`.js"`, `.min.js"`);
+  file = file.replaceAll(`.js"`, `.min.js"`);
 
-fs.writeFileSync("./dist/index.html", file);
+  fs.writeFileSync("./dist/index.html", file);
+}
+{
+  let file = fs.readFileSync("./dist/service-worker.js").toString();
+
+  file = file.replaceAll(`.js"`, `.min.js"`);
+
+  fs.writeFileSync("./dist/service-worker.js", file);
+}
