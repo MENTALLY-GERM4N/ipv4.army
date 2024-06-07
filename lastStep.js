2210904bbe21e740267bd3acc0b4e32c6409ba3f
Unstream <drns.pw@icloud.com>
2024-06-07 16:31:24 -0400

    sobbing

--- lastStep.js
+++ lastStep.js
@@ -4,6 +4,7 @@ import fs from "fs";
   let file = fs.readFileSync("./dist/index.html").toString();
 
   file = file.replaceAll(`.js"`, `.min.js"`);
+  file = file.replaceAll(`service-worker.min.js"`, `service-worker.js"`);
 
   fs.writeFileSync("./dist/index.html", file);
 }
