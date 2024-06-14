ba635dc44a3a1009aa7278d889a9bb805814350f
Unstream <drns.pw@icloud.com>
2024-06-14 01:15:41 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,160 @@
 import { serve, file } from "bun";
+import { defaultTransformers } from "@lilybird/transformers";
+import { createClient, Intents } from "lilybird";
+import albumArt from "./albumArt.js";
+
+import die from "./die.js";
+
+import { randomUUID } from "node:crypto";
+import { EventEmitter } from "node:events";
+
+const clients = [];
+let userData = {
+  user: {},
+  status: "offline",
+  client_status: {},
+  broadcast: null,
+  activities: [],
+};
+
+await createClient({
+  token: process.env.TOKEN,
+  intents: [Intents.GUILD_MEMBERS, Intents.GUILD_PRESENCES],
+  transformers: defaultTransformers,
+  listeners: {
+    ready: async (client) => {
+      console.log(`Logged in as ${client.user.username}`);
+
+      userData.user = await client.rest.getUser(process.env.USERID);
+    },
+    presenceUpdate: async (_, data) => {
+      if (data.user.id !== process.env.USERID) return;
+
+      delete data.guild_id;
+      delete data.user;
+
+      Object.assign(userData, data);
+
+      return await sendWebSocketMessage();
+    },
+    guildMemberUpdate: async (client) => {
+      userData.user = await client?.rest?.getUser(process.env.USERID);
+
+      return await sendWebSocketMessage();
+    },
+  },
+});
+
+const sendWebSocketMessage = async () => {
+  const sendPromises = clients.map((ws) => {
+    return ws.send(JSON.stringify(userData));
+  });
+
+  return await Promise.all(sendPromises);
+};
+
+const opts = {
+  headers: {
+    "Cache-Control": "public, max-age=31536000, immutable",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
+
+const defaultImage =
+  "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+
+const sseEvents = new EventEmitter();
+
+const sse = (data) => {
+  sseEvents.emit(
+    "sse",
+    `id: ${randomUUID()}\ndata: ${JSON.stringify(data)}\n\n`
+  );
+};
+
+let counter = 0;
+setInterval(() => {
+  sse({ payload: { date: Date.now(), times: counter++ } });
+});
 
 serve({
   fetch: async (req, server) => {
     const path = new URL(req.url);
-    return new Response(
-      file(`.${path.pathname == "/" ? "/index.html" : path.pathname}`)
+
+    // :)
+    if (path.pathname == "/.env") {
+      return new Response(
+        new ReadableStream({
+          start(controller) {
+            const send = async () => {
+              controller.enqueue(await die(performance.now().toString()));
+            };
+
+            setInterval(send);
+          },
+        }),
+        {
+          headers: {
+            "Content-Type": "text/plain",
+            "Cache-Control": "no-cache, no-transform",
+            Connection: "keep-alive",
+            "X-Accel-Buffering": "no",
+          },
+        }
+      );
+    }
+
+    let params = "";
+    const url = path.searchParams.get("url");
+    const w = path.searchParams.get("w");
+    const h = path.searchParams.get("h");
+    const output = path.searchParams.get("output");
+    if (w) params += `&w=${w}`;
+    if (h) params += `&h=${h}`;
+    if (output) params += `&w=${output}`;
+
+    if (path.pathname == "/ws" && server.upgrade(req)) {
+      return;
+    } else if (path.pathname == "/img") {
+      if (url) {
+        let fetchResp = await fetch(`https://wsrv.nl/?url=${url}${params}`);
+        let buffer = await fetchResp.arrayBuffer();
+        return new Response(buffer, opts);
+      } else {
+        let buffer = Uint8Array.from(atob(defaultImage), (c) =>
+          c.charCodeAt(0)
+        );
+        resp = new Response(buffer, opts);
+      }
+    } else if (path.pathname == "/albumArt") {
+      let fetchResp = await fetch(
+        `https://wsrv.nl/?url=${await albumArt(
+          path.searchParams.get("q") || ""
+        )}${params}`
+      );
+      let buffer = await fetchResp.arrayBuffer();
+      return new Response(buffer, opts);
+    }
+
+    const fileRes = file(
+      `.${path.pathname == "/" ? "/index.html" : path.pathname}`
     );
+
+    let text = await fileRes.text();
+
+    text = text.replace("{ DISCORD_USER_DATE: {} }", JSON.stringify(userData));
+
+    return new Response(text, {
+      headers: {
+        "content-type": fileRes.type,
+      },
+    });
+  },
+  websocket: {
+    open: async (ws) => {
+      clients.push(ws);
+      ws.send(JSON.stringify(userData));
+    },
+    close: async (ws) => clients.pop(ws),
   },
 });
---
+++ main.js
@@ -0,0 +1,59 @@
+import ui from "./node_modules/beercss/dist/cdn/beer.js";
+import "./node_modules/material-dynamic-colors/dist/cdn/material-dynamic-colors.min.js";
+
+let pfp = document.getElementById("pfp");
+let username = document.getElementById("username");
+let musicInt = document.getElementById("musicInt");
+let art = document.getElementById("art");
+let music = document.getElementById("music");
+
+const statusColors = {
+  online: "#4caf50",
+  idle: "#ffeb3b",
+  dnd: "#f44336",
+  offline: "#9e9e9e",
+};
+
+const discord = new WebSocket(
+  `${location.protocol.replace("http", "ws")}//${location.host}/ws`
+);
+
+discord.onmessage = async ({ data }) => {
+  return await handleEvent(JSON.parse(data));
+};
+
+const handleEvent = async (data) => {
+  pfp.src = `/img?url=https://cdn.discordapp.com/avatars/1125315673829154837/${data.user.avatar}.webp&w=96&h=96&output=webp`;
+  username.innerText = data.user.username;
+
+  let appleMusic = data.activities.filter((act) => {
+    return act.application_id == "842112189618978897";
+  });
+  if (appleMusic.length > 0) {
+    appleMusic = appleMusic[0];
+
+    if (appleMusic.assets.large_image.startsWith("mp:external/")) {
+      appleMusic.assets.large_image = `/img?url=https://media.discordapp.net/external/${appleMusic.assets.large_image.replace(
+        "mp:external/",
+        ""
+      )}&w=96&h=96&output=webp`;
+    } else {
+      const reqParam = encodeURIComponent(
+        `${appleMusic.details} ${appleMusicassets.large_text} ${appleMusic.state}`
+      )
+        .replace(/"/g, "%27")
+        .replace(/"/g, "%22");
+      appleMusic.assets.large_image = `/albumArt?q=${reqParam}&w=96&h=96&output=webp`;
+    }
+
+    art.src = appleMusic.assets.large_image;
+    music.innerText = `${appleMusic.details} by ${appleMusic.state}`;
+    await ui("theme", appleMusic.assets.large_image);
+    musicInt.style.display = "";
+  } else {
+    await ui("theme", statusColors[data.status]);
+    musicInt.style.display = "none";
+  }
+};
+
+handleEvent({ DISCORD_USER_DATE: {} });
