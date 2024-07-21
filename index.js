01f1e3462adf5079ad9b02508e4f095b2a80d215
wont.stream <drns.pw@icloud.com>
2024-07-20 22:50:36 -0400

    more clean up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { serve, file } from "bun";
-import {readdirSync} from "fs"
+import { readdirSync } from "fs";
 import { defaultTransformers } from "@lilybird/transformers";
 import { createClient, Intents } from "lilybird";
 import { minify } from "uglify-js";
@@ -95,42 +95,48 @@ const app = {
   },
 };
 
-app.ws.open("/api/ws", async (ws) =>  {
+app.ws.open("/api/ws", async (ws) => {
   clients.push(ws);
   ws.send(JSON.stringify(userData));
-})
+});
 
-app.ws.close("/api/ws", async (ws) =>  {
+app.ws.close("/api/ws", async (ws) => {
   clients.pop(ws);
-})
+});
 
-app.get("/api/img", async (req, server) =>  {
-  const imgReq = await fetch(`https://wsrv.nl/${new URL(req.url).search}`)
-  const img = await imgReq.arrayBuffer()
-  
+app.get("/api/img", async (req, server) => {
+  const imgReq = await fetch(`https://wsrv.nl/${new URL(req.url).search}`);
+  const img = await imgReq.arrayBuffer();
+
   return new Response(Bun.gzipSync(img), opts);
-})
+});
 
 const node_modules = [
-  ...readdirSync("./node_modules", { recursive: true }).map((f) => {return `./node_modules/${f.replaceAll("\\", "/")}`})
-]
+  ...readdirSync("./node_modules", { recursive: true }).map((f) => {
+    return `./node_modules/${f.replaceAll("\\", "/")}`;
+  }),
+];
 
-node_modules.forEach(fileName =>  {
-  app.get(fileName.replace(".", ""), async (req, server) =>  {
-    return new Response(file(fileName))
-  })
-})
+node_modules.forEach((fileName) => {
+  app.get(fileName.replace(".", ""), async (req, server) => {
+    return new Response(file(fileName));
+  });
+});
 
 const src = [
-  ...readdirSync("./src", { recursive: true }).map((f) => {return `./src/${f.replaceAll("\\", "/")}`})
-]
+  ...readdirSync("./src", { recursive: true }).map((f) => {
+    return `./src/${f.replaceAll("\\", "/")}`;
+  }),
+];
 
-src.forEach(fileName =>  {
-  app.get(fileName.replace("./src/", "/").replace("index.html", ""), async (req, server) =>  {
-    console.log(fileName)
-    return new Response(file(fileName))
-  })
-})
+src.forEach((fileName) => {
+  app.get(
+    fileName.replace("./src/", "/").replace("index.html", ""),
+    async (req, server) => {
+      return new Response(file(fileName));
+    }
+  );
+});
 
 await createClient({
   token: process.env.TOKEN,
@@ -168,14 +174,13 @@ const sendWebSocketMessage = async () => {
   return await Promise.all(sendPromises);
 };
 
-
 const server = Bun.serve({
   async fetch(req, server) {
     const path = new URL(req.url).pathname;
 
     if (server.upgrade(req, { data: { path } })) return undefined;
 
-    let cb = app.routes[req.method][path]
+    let cb = app.routes[req.method][path];
 
     if (cb) return cb(req, server);
 
--- test.js
+++
@@ -1,3 +0,0 @@
-const { readdirSync } = require("fs")
-
-console.log(readdirSync("./node_modules", { recursive: true }).map((f) => {return `./src/${f.replaceAll("\\", "/")}`}))
