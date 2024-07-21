64c8fc0429df7c00551c7984cdfa325617a66363
wont.stream <drns.pw@icloud.com>
2024-07-21 03:50:08 -0400

    update

---
+++ src/1.js
@@ -0,0 +1,9 @@
+fetch("https://1.ipv4.army/api/event", {
+  method: "POST",
+  body: JSON.stringify({
+    domain: "ipv4.army",
+    name: "pageview",
+    url: location.href,
+    referrer: document.referrer,
+  }),
+});
