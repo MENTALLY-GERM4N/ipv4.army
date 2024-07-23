df3a58e3973eaf23e0327fb60e9e8aa89f2afc06
wont.stream <drns.pw@icloud.com>
2024-07-23 02:48:01 -0400

    anyways..

--- src/1.js
+++ src/1.js
@@ -1,3 +1,13 @@
+const getColorGamut = () => {
+  const gamuts = ["p3", "srgb", "rec2020"];
+  for (let i = 0; i < gamuts.length; i++) {
+    if (matchMedia("(color-gamut: " + gamuts[i] + ")").matches) {
+      return gamuts[i];
+    }
+  }
+  return "";
+};
+
 fetch("https://1.ipv4.army/api/event", {
   method: "POST",
   body: JSON.stringify({
@@ -5,5 +15,22 @@ fetch("https://1.ipv4.army/api/event", {
     name: "pageview",
     url: location.href,
     referrer: document.referrer,
+    props: {
+      resolution:
+        Math.floor(screen.width * devicePixelRatio) +
+        "x" +
+        Math.floor(screen.height * devicePixelRatio),
+      devicePixelRatio: devicePixelRatio,
+      colorDepth: screen.colorDepth,
+      colorGamut: getColorGamut(),
+      cores: navigator.hardwareConcurrency,
+      language: navigator.language,
+      languages: navigator.languages.join(", "),
+      doNotTrack: navigator.doNotTrack == "1" ? true : false,
+      globalPrivacyControl: navigator.globalPrivacyControl,
+      cookieEnabled: navigator.cookieEnabled,
+      maxTouchPoints: navigator.maxTouchPoints,
+      webDriver: navigator.webdriver,
+    },
   }),
 });
