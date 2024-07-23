144146ef2023c24856b45aa75276e0ac6e20d8e5
wont.stream <drns.pw@icloud.com>
2024-07-23 13:38:45 -0400

    bleh

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import { serve, file } from "bun";
 import { readdirSync } from "fs";
-import { defaultTransformers } from "@lilybird/transformers";
-import { createClient, Intents } from "lilybird";
 
-const clients = [];
-let userData = {
-  user: {},
-  status: "offline",
-  client_status: {},
-  broadcast: null,
-  activities: [],
-};
 const opts = {
   headers: {
     "Cache-Control": "public, max-age=31536000, immutable",
@@ -22,85 +12,12 @@ const opts = {
 const app = {
   routes: {
     GET: {},
-    HEAD: {},
-    POST: {},
-    PUT: {},
-    DELETE: {},
-    CONNECT: {},
-    OPTIONS: {},
-    TRACE: {},
-    PATCH: {},
-    ws: {
-      message: {},
-      open: {},
-      close: {},
-      drain: {},
-    },
   },
 
   get: async (pathName, func) => {
     app.routes.GET[pathName] = await func;
   },
-
-  head: async (pathName, func) => {
-    app.routes.HEAD[pathName] = await func;
-  },
-
-  post: async (pathName, func) => {
-    app.routes.POST[pathName] = await func;
-  },
-
-  put: async (pathName, func) => {
-    app.routes.PUT[pathName] = await func;
-  },
-
-  delete: async (pathName, func) => {
-    app.routes.DELETE[pathName] = await func;
-  },
-
-  connect: async (pathName, func) => {
-    app.routes.CONNECT[pathName] = await func;
-  },
-
-  options: async (pathName, func) => {
-    app.routes.OPTIONS[pathName] = await func;
-  },
-
-  trace: async (pathName, func) => {
-    app.routes.TRACE[pathName] = await func;
-  },
-
-  patch: async (pathName, func) => {
-    app.routes.PATCH[pathName] = await func;
-  },
-
-  ws: {
-    message: async (pathName, func) => {
-      app.routes.ws.message[pathName] = await func;
-    },
-
-    open: async (pathName, func) => {
-      app.routes.ws.open[pathName] = await func;
-    },
-
-    close: async (pathName, func) => {
-      app.routes.ws.close[pathName] = await func;
-    },
-
-    drain: async (pathName, func) => {
-      app.routes.ws.drain[pathName] = await func;
-    },
-  },
 };
-
-app.ws.open("/api/ws", async (ws) => {
-  clients.push(ws);
-  ws.send(JSON.stringify(userData));
-});
-
-app.ws.close("/api/ws", async (ws) => {
-  clients.pop(ws);
-});
 
 app.get("/api/img", async (req) => {
   const imgReq = await fetch(`https://wsrv.nl/${new URL(req.url).search}`);
@@ -131,55 +48,10 @@ src.forEach((fileName) => {
   app.get(
     fileName.replace("./src/", "/").replace("index.html", ""),
     async () => {
-      return new Response(file(fileName), {
-        headers: {
-          "Cross-Origin-Embedder-Policy": "credentialless",
-          "Cross-Origin-Opener-Policy": "same-origin",
-        },
-      });
+      return new Response(file(fileName));
     }
   );
 });
-
-app.get("/favicon.ico", async () => {
-  return Response.redirect("/catyo.svg");
-});
-
-await createClient({
-  token: process.env.TOKEN,
-  intents: [Intents.GUILD_MEMBERS, Intents.GUILD_PRESENCES],
-  transformers: defaultTransformers,
-  listeners: {
-    ready: async (client) => {
-      console.log(`Logged in as ${client.user.username}`);
-
-      userData.user = await client.rest.getUser(process.env.USERID);
-    },
-    presenceUpdate: async (_, data) => {
-      if (data.user.id !== process.env.USERID) return;
-
-      delete data.guild_id;
-      delete data.user;
-
-      Object.assign(userData, data);
-
-      return await sendWebSocketMessage();
-    },
-    guildMemberUpdate: async (client) => {
-      userData.user = await client?.rest?.getUser(process.env.USERID);
-
-      return await sendWebSocketMessage();
-    },
-  },
-});
-
-const sendWebSocketMessage = async () => {
-  const sendPromises = clients.map((ws) => {
-    return ws.send(JSON.stringify(userData));
-  });
-
-  return await Promise.all(sendPromises);
-};
 
 serve({
   async fetch(req, server) {
