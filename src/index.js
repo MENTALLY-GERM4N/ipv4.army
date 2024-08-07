5064350ef133b944521a6b367f06b259447c2944
Zenith <143244075+wont-stream@users.noreply.github.com>
2024-08-06 22:30:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.querySelectorAll("i").forEach(async (icon) => {
   let svg = await fetch(
-    `https://cdn.jsdelivr.net/npm/@material-symbols/svg-400/rounded/${icon.innerText}-fill.min.svg`
+    `https://cdn.jsdelivr.net/npm/@material-symbols/svg-400/rounded/${icon.innerText}-fill.svg`
   );
 
   icon.innerHTML = await svg.text();
