113fb3dd4f89a621c2d6f5b3986e8885a37db8c7
wont.stream <drns.pw@icloud.com>
2024-09-17 00:50:17 -0400

    ?

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ import { serve, file } from "bun";
 
 serve({
   static: {
-    "/index.css": new Response(await file("./src/index.css").bytes()),
-    "/index.js": new Response(await file("./dist/index.js").bytes()),
+    "/index.css": new Response(await file("./src/index.css").bytes(), {
+      headers: {
+        "Content-Type": "text/css",
+      },
+    }),
+    "/index.js": new Response(await file("./dist/index.js").bytes(), {
+      headers: {
+        "Content-Type": "text/javascript",
+      },
+    }),
 
-    "/index.html": new Response(await file("./src/index.html").bytes()),
+    "/": new Response(await file("./src/index.html").bytes(), {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    }),
 
-    "/manifest.json": new Response(await file("./src/manifest.json").bytes()),
-    "/robots.txt": new Response(await file("./src/robots.txt").bytes()),
+    "/manifest.json": new Response(await file("./src/manifest.json").bytes(), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }),
+    "/robots.txt": new Response(await file("./src/robots.txt").bytes(), {
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }),
   },
   async fetch(req, server) {
     return Response.redirect("https://ipv4.army", 307);
