82fd6bf16cbc521a3a4564967140fbc0316e6b8d
wont.stream <drns.pw@icloud.com>
2024-09-17 01:00:45 -0400

    sigh

--- src/index.js
+++ src/index.js
@@ -36,10 +36,11 @@ document.getElementById("music").append(
             `background: url(https://wsrv.nl/?output=webp&q=1&url=${tidal.album_art_url.replace(
               "80x80",
               "1280x1280"
-            )}) center center / cover no-repeat; filter: drop-shadow(1px 1px 20px ${
-              tidal.color
-            });`
+            )}) center center / cover no-repeat;`
           );
+        document.getElementById(
+          "blur"
+        ).style.filter = `drop-shadow(1px 1px 20px ${tidal.color});`;
         document.getElementById(
           "link"
         ).href = `https://tidal.com/browse/track/${tidal.track_id}/u`;
