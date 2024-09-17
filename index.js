b386e14c8225bcf09f20e8e64f25a743cff46b45
wont.stream <drns.pw@icloud.com>
2024-09-17 00:51:10 -0400

    last thing

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ serve({
   static: {
     "/index.css": new Response(await file("./src/index.css").bytes(), {
       headers: {
-        "Content-Type": "text/css",
+        "Content-Type": "text/css; charset=utf-8",
       },
     }),
     "/index.js": new Response(await file("./dist/index.js").bytes(), {
       headers: {
-        "Content-Type": "text/javascript",
+        "Content-Type": "text/javascript; charset=utf-8",
       },
     }),
 
     "/": new Response(await file("./src/index.html").bytes(), {
       headers: {
-        "Content-Type": "text/html",
+        "Content-Type": "text/html; charset=utf-8",
       },
     }),
 
     "/manifest.json": new Response(await file("./src/manifest.json").bytes(), {
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
       },
     }),
     "/robots.txt": new Response(await file("./src/robots.txt").bytes(), {
       headers: {
-        "Content-Type": "text/plain",
+        "Content-Type": "text/plain; charset=utf-8",
       },
     }),
   },
