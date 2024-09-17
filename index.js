5177b1bd19f11b3de488a8db49738e40ff0dd63f
wont.stream <drns.pw@icloud.com>
2024-09-17 00:46:52 -0400

    oops..

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { serve, file } from "bun";
 
 serve({
   static: {
-    "/index.css": await file("./src/index.css").bytes(),
-    "/index.js": await file("./dist/index.js").bytes(),
+    "/index.css": new Response(await file("./src/index.css").bytes()),
+    "/index.js": new Response(await file("./dist/index.js").bytes()),
 
-    "/": await file("./src/index.html").bytes(),
+    "/index.html": new Response(await file("./src/index.html").bytes()),
 
-    "/manifest.json": await file("./src/manifest.json").bytes(),
-    "/robots.txt": await file("./src/robots.txt").bytes(),
+    "/manifest.json": new Response(await file("./src/manifest.json").bytes()),
+    "/robots.txt": new Response(await file("./src/robots.txt").bytes()),
   },
   async fetch(req, server) {
     return Response.redirect("https://ipv4.army", 307);
