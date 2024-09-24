5440682d814d75b78a61e617e9aea484b878b69f
wont.stream <drns.pw@icloud.com>
2024-09-24 01:32:50 -0400

    updates

--- build.ts
+++ build.ts
@@ -6,11 +6,14 @@ import postcssImport from "postcss-import";
 
 import { $, build, file, write } from "bun";
 
-await $`rm -rf ./dist`;
+try {
+	await $`rm -rf ./dist`;
+} catch (_) {}
+
+let html = await file("./src/index.html").text();
 
 const js = await build({
 	entrypoints: ["./src/index.tsx"],
-	outdir: "./dist",
 	format: "esm",
 	minify: true,
 });
@@ -19,30 +22,27 @@ if (!js.success) {
 	process.exit(1);
 }
 
-await postcss([
+html = html.replace(
+	`<script src="./index.tsx"></script>`,
+	`<script>${await js.outputs[0].text()}</script>`,
+);
+
+const postCss = postcss([
 	autoprefixer(),
 	postcssImport(),
 	postcssPurge({
 		content: ["./src/index.html", "./dist/index.js"],
 	}),
 	cssnano({ preset: "default" }),
-])
-	.process(await file("./src/index.css").text(), {
-		from: "./src/index.css",
-		to: "./dist/index.css",
-	})
-	.then((result) => {
-		write("./dist/index.css", result.css);
-	});
+]);
+const { css } = await postCss.process(await file("./src/index.css").text(), {
+	from: "./src/index.css",
+	to: "./dist/index.css",
+});
 
-let html = await file("./src/index.html").text();
 html = html.replace(
 	`<link href="./index.css" rel="stylesheet" />`,
-	`<style>${await file("./dist/index.css").text()}</style>`,
-);
-html = html.replace(
-	`<script src="./index.tsx"></script>`,
-	`<script>${await file("./dist/index.js").text()}</script>`,
+	`<style>${css}</style>`,
 );
 
 await write("./dist/index.html", html);
--- index.ts
+++ index.ts
@@ -3,29 +3,11 @@ import { file, serve } from "bun";
 import "./build.ts";
 
 serve({
-	static: {
-		"/": new Response(await file("./dist/index.html").bytes(), {
-			headers: {
-				"Content-Type": "text/html; charset=utf-8",
-			},
-		}),
-
-		"/manifest.json": new Response(await file("./src/manifest.json").bytes(), {
-			headers: {
-				"Content-Type": "application/json; charset=utf-8",
-			},
-		}),
-		"/robots.txt": new Response(await file("./src/robots.txt").bytes(), {
-			headers: {
-				"Content-Type": "text/plain; charset=utf-8",
-			},
-		}),
-	},
 	async fetch(req, server) {
 		const { pathname } = new URL(req.url);
 
 		// Static doesn't work right on windows?
-		if (process.env.ISDEV === "true" && pathname === "/") {
+		if (pathname === "/") {
 			return new Response(await file("./dist/index.html").bytes(), {
 				headers: {
 					"Content-Type": "text/html; charset=utf-8",
